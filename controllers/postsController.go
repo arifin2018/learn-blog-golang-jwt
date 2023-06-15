0db016a3ae180dcfd19b9de0baee37d28277e3a3
arifinlenna <arifin@lenna.ai>
2023-06-15 07:20:24 +0700

    transaction db post

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"Blog/helpers"
 	"Blog/models"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -22,6 +23,7 @@ func CreatePosts(c *gin.Context)  {
 	db := c.MustGet("db").(*gorm.DB)
 
 	var Post models.Post
+	var Tag models.Tag
 
 	if err := c.ShouldBindJSON(&Post); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -34,20 +36,51 @@ func CreatePosts(c *gin.Context)  {
         return
 	}
 	inputPost := models.Post{Title: Post.Title ,Content: Post.Content, User_id: int(user_id), Created_at: time.Now()}
-	db.Debug().Create(&inputPost)
+	
+	tx := db.Begin()
+	if err := tx.Error; err != nil {
+		c.JSON(http.StatusOK, gin.H{"err": err})
+		return
+	}
+	if err := tx.Debug().Create(&inputPost).Error; err != nil {
+		tx.Rollback()
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"err": "failed to create input post"})
+		return
+	}
+	
 	if len(Post.Image_url) > 0 {
 		for _, v := range Post.Image_url {
 			ImageCreate := models.Image{Image_url: v}
-			db.Debug().Create(&ImageCreate)
+			if err := tx.Debug().Create(&ImageCreate).Error; err !=nil {
+				tx.Rollback()
+				c.JSON(http.StatusOK, gin.H{"err": err})
+				return
+			}
+			
 			PostImage := models.PostImage{PostId: inputPost.ID,ImageId: ImageCreate.ID}
-			db.Debug().Create(&PostImage)
+			if err := tx.Debug().Create(&PostImage).Error; err !=nil {
+				tx.Rollback()
+				c.JSON(http.StatusOK, gin.H{"err": err})
+				return
+			}
 		}
 	}
 	if len(Post.TagId) > 0 {
 		for _, v := range Post.TagId {
 			PostTag := models.PostTag{PostId: inputPost.ID, TagId: v}
-			db.Debug().Create(&PostTag)
+			if err := db.Find(&Tag,v).Error; err != nil {
+				tx.Rollback()
+				c.JSON(http.StatusOK, gin.H{"err": "Tag not found"})
+				return
+			}
+			if err := tx.Debug().Create(&PostTag).Error; err !=nil {
+				tx.Rollback()
+				c.JSON(http.StatusOK, gin.H{"err": err})
+				return
+			}
 		}
 	}
+	tx.Commit()
 	c.JSON(http.StatusOK, gin.H{"data": inputPost})
 }
