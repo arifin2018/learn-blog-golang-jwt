62bb7a0e963193d867c0497732d289e1b2c0a742
arifinlenna <arifin@lenna.ai>
2023-06-15 10:44:40 +0700

    upload .env and configur db

--- config/config.go
+++ config/config.go
@@ -1,19 +1,25 @@
 package config
 
 import (
+	"Blog/helpers"
 	"Blog/models"
 	"fmt"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+var username = helpers.Getenv("username", "root")
+var password = helpers.Getenv("password", "123456")
+var host = helpers.Getenv("host", "127.0.0.1")
+var port = helpers.Getenv("port", "3306")
+var database = helpers.Getenv("database", "blogGO")
 
 func ConnectDB() *gorm.DB {
-	username := "root"
-	password := "123456"
-	host 	 := "127.0.0.1"
-	port 	 := "3306"
-	database := "blogGO"
+	// username := "root"
+	// password := "123456"
+	// host 	 := "127.0.0.1"
+	// port 	 := "3306"
+	// database := "blogGO"
 
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",username,password,host,port,database)
   	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
