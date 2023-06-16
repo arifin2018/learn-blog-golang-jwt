55f882b2c0520647e299fe95620556584a8883f9
arifinlenna <arifin@lenna.ai>
2023-06-16 09:28:47 +0700

    delete drop column

--- config/config.go
+++ config/config.go
@@ -4,7 +4,6 @@ import (
 	"Blog/helpers"
 	"Blog/models"
 	"fmt"
-	"log"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -32,9 +31,5 @@ func ConnectDB() *gorm.DB {
 
 	db.AutoMigrate(&models.User{},&models.Post{}, &models.Tag{},&models.Image{},&models.Comment{})
 
-	if err := db.Migrator().DropColumn(&models.Comment{}, "image_url"); err != nil{
-		// Do whatever you want to do!
-		log.Print("ERROR: We expect the description column to be drop-able")
-	}
 	return db
 }
